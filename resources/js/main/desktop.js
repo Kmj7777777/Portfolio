8132c509234997cdfcd9bdb1c588b0e972e0ddb9
kmj950728 <dngu_icdi@naver.com>
2022-06-14 03:14:39 +0900

    Portfolio

---
+++ resources/js/main/desktop.js
@@ -0,0 +1,90 @@
+$(function(){
+	$.ajaxSetup({
+		async:false
+	});
+	
+	$.ajax({
+		url:"/Portfolio/resources/json/main/desktop.json",
+		
+		success:function(json){
+			$.each(json.list, function(index, file){
+				$("#desktop > ul").append("<li id='" + file.name.toLowerCase() + "' class='file'>" +
+														"<img src='" + file.icon + "'><br>" +
+														"<span>" + file.name + "</span>" +
+													"</li>");
+			});
+		}
+	});
+	
+	$("#desktop").fadeIn();
+});
+
+$(function(){
+	$("body").on(getClickType(), "#desktop .file", function(e, isForCopying){
+		var type = $(this).attr("id");
+		
+		var uuid = create_window(type, isForCopying);
+		$(this).data("uuid", uuid);
+		$("#" + uuid + " > #body > .content").attr("id", type);
+		
+		if(type == "about"){
+			$("#" + uuid + " > #body > .content").append("<h1>김민진에 대한 기본 정보 보기</h1>" +
+																		  "<ul>" +
+																		  	  "<li class='profile'>" +
+																		  	  	  "<p class='boundary'><span>Profile</span></p>" +
+																		  	  	  "<div>" +
+																		  	  	  	  "<h1>김민진</h1>" +
+																		  	  	  	  "<p>Back-End 개발자(신입)</p>" +
+																		  	  	  	  "<p>1995.07.28</p>" +
+																		  	  	  	  "<p>인천광역시 부평구</p>" +
+																		  	  	  	  "<p>dngu_icdi@naver.com</p>" +
+																		  	  	  "</div>" +
+																		  	  	  "<img src='/Portfolio/resources/images/main/about/me.jpg'>" +
+																		  	  "</li>" +
+																		  	  "<li class='experience'>" +
+																		  	  	  "<p class='boundary'><span>Programming Experience</span></p>" +
+																		  	  	  "<ul></ul>" +
+																		  	  "</li>" +
+																		  	  "<li class='skills'>" +
+																		  	  	  "<p class='boundary'><span>Skills</span></p>" +
+																		  	  	  "<ul></ul>" +
+																		  	  "</li>" +
+																		  "</ul>");
+			
+			$.ajax({
+				url:"/Portfolio/resources/json/main/about.json",
+				
+				success:function(json){
+					$.each(json.experience, function(index, experience){
+						$("#" + uuid + " > #body > .content .experience > ul").append("<li>" +
+																												"<h2>" + experience.period + "</h2>" +
+																												"<i> : </i>" +
+																												"<span>" + experience.process + "</span>" +
+																											"</li>");
+					});
+					
+					$.each(json.skills, function(key, value){
+						$.each(value, function(i, val){
+							var class_ = key;
+							if(i == 0){
+								if(key == "server_side"){
+									$("#" + uuid + " > #body > .content .skills > ul").append("<li class='contour'></li>");
+								}
+								
+								class_ += " first";
+							}else if(i == value.length - 1){
+								class_ += " last";
+							}
+							
+							$("#" + uuid + " > #body > .content .skills > ul").append("<li class='" + class_ + "'><img src='" + val.icon + "'></li>");
+						});
+					});
+				}
+			});
+		}else if(type == "project"){
+			compose_directory(uuid, "project", "/Portfolio/resources/json/main/project.json", false);
+		}else if(type == "contact"){
+			compose_directory(uuid, "contact", "/Portfolio/resources/json/main/contact.json", false);
+		}
+	});
+});
